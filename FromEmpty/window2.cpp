1a1f5bcdb216a31e09eeee16825dbe1c83270bcb
waltron0 <waltonti00@gmail.com>
2016-05-09 07:25:10 -0600

    ya

--- FromEmpty/window2.cpp
+++ FromEmpty/window2.cpp
@@ -1,20 +1,24 @@
 #include "window2.h"
 #include <QPushButton>
 #include <QApplication>
+#include <QLCDNumber>
 
 Window2::Window2(QWidget *parent) : QWidget(parent)
 {
     //Set window size:
-    setFixedSize(500,250);//comment this out and it should work
+    setFixedSize(500,250);
 
     //create and position the button
     //I guess you don't even need a layout... it just puts the button in there
-    baton = new QPushButton("baton",this);//comment this out and it should work
-    baton->setGeometry(10,10,80,30);//comment this out and it should work
+    baton = new QPushButton("baton",this); // is it making itself (the button) the parent, or does it refer to the Window2?
+    baton->setGeometry(10,10,80,30);
 
-    quitButton = new QPushButton("Quit",this);//comment this out and it should work
-    quitButton->setGeometry(420,220,80,30);//comment this out and it should work
-    QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));//comment this out and it should work
+    quitButton = new QPushButton("Quit",this);
+    quitButton->setGeometry(420,220,80,30);
+    QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
 
-    //now just try to make baton do something...
+    numbLCD = new QLCDNumber();
+    numbLCD->setGeometry(30,30,40,40);
+    numbLCD->display(4);
+    //statusBar->addWidget(numbLCD);
 }
--- FromEmpty/window2.h
+++ FromEmpty/window2.h
@@ -3,15 +3,17 @@
 
 #include <QWidget>
 
-class QPushButton; //comment this out and it should work  (I don't understand this ..... I mean, this class is already defined, and why not just do #include <QPushButton> ?
+class QPushButton; // (I don't understand this ..... I mean, this class is already defined, and why not just do #include <QPushButton> ?
+class QLCDNumber; // same, trying manually
 class Window2 : public QWidget
 {
     Q_OBJECT
 public:
     explicit Window2(QWidget *parent = 0);
-private://comment this out and it should work
-    QPushButton *baton;//comment this out and it should work
-    QPushButton *quitButton;//comment this out and it should work
+private:
+    QPushButton *baton;
+    QPushButton *quitButton;
+    QLCDNumber *numbLCD;
 signals:
 
 public slots:
